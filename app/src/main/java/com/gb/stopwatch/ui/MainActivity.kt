fc3fb4d5c5ceda8e175918ad5c042611dd67476e
Mikhail <m.kopylov@arammeem.com>
2022-04-10 16:44:48 +0300

    Home work 4. Part 2.

--- app/src/main/java/com/gb/stopwatch/ui/MainActivity.kt
+++ app/src/main/java/com/gb/stopwatch/ui/MainActivity.kt
@@ -9,25 +9,65 @@ import com.gb.stopwatch.presentation.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: MainActivityViewModel = MainActivityViewModel()
+    private val viewModelFirst: MainActivityViewModel = MainActivityViewModel()
+    private val viewModelSecond: MainActivityViewModel = MainActivityViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initStopwatchFirst()
+        initStopwatchSecond()
+        initStopwatchAll()
+    }
+
+    private fun initStopwatchFirst() {
         val textView = findViewById<TextView>(R.id.text_time)
-        viewModel.liveData.observe(this) {
+        viewModelFirst.liveData.observe(this) {
             textView.text = it
         }
 
         findViewById<Button>(R.id.button_start).setOnClickListener {
-            viewModel.start()
+            viewModelFirst.start()
         }
         findViewById<Button>(R.id.button_pause).setOnClickListener {
-            viewModel.pause()
+            viewModelFirst.pause()
         }
         findViewById<Button>(R.id.button_stop).setOnClickListener {
-            viewModel.stop()
+            viewModelFirst.stop()
+        }
+    }
+
+
+    private fun initStopwatchSecond() {
+        val textView = findViewById<TextView>(R.id.text_time_2)
+        viewModelSecond.liveData.observe(this) {
+            textView.text = it
+        }
+
+        findViewById<Button>(R.id.button_start_2).setOnClickListener {
+            viewModelSecond.start()
+        }
+        findViewById<Button>(R.id.button_pause_2).setOnClickListener {
+            viewModelSecond.pause()
+        }
+        findViewById<Button>(R.id.button_stop_2).setOnClickListener {
+            viewModelSecond.stop()
+        }
+    }
+
+    private fun initStopwatchAll() {
+        findViewById<Button>(R.id.button_start_all).setOnClickListener {
+            viewModelSecond.start()
+            viewModelFirst.start()
+        }
+        findViewById<Button>(R.id.button_pause_all).setOnClickListener {
+            viewModelSecond.pause()
+            viewModelFirst.pause()
+        }
+        findViewById<Button>(R.id.button_stop_all).setOnClickListener {
+            viewModelSecond.stop()
+            viewModelFirst.stop()
         }
     }
 }
